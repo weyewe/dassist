4b0dab6af4ce34202596f90166f7c89f5c40c934
weyewe <w.yunnal@gmail.com>
2011-06-06 20:07:47 +0700

    starter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dassist::Application.config.secret_token = '20508da35502021cf5b163a71db1d4455e494599ee9d62eafb30de2b87a54d65b7c92c48623ef81e7672d2d9e5d2c1274e868669ab27a71de770923ddb1e74bc'
